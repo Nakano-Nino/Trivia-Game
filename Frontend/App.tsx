e1ec2de18c46ee3ffe4221ee4c26f6b91175e4cf
wira97-tech <leonkmoena@gmail.com>
2024-01-09 08:33:30 +0700

    avatar

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="LandingPage">
+        <Stack.Navigator initialRouteName="Profile">
           <Stack.Screen
             name="SplashScreen"
             component={SplashScreen}
--- Frontend/pages/Profile.tsx
+++ Frontend/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { StatusBar } from "expo-status-bar"
 import {
   Image,
@@ -13,8 +13,7 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native-gesture-handler"
-import { MaterialIcons } from "@expo/vector-icons"
-import Avatar from "../components/Avatar"
+import axios, { Axios } from "axios"
 
 interface DataAvatar {
   id: string
@@ -23,23 +22,49 @@ interface DataAvatar {
 }
 const Profile = () => {
   const data: DataAvatar[] = [
-    { id: "1", name: "John", imageUrl: "../assets/avatar1.png" },
-    { id: "2", name: "Jane", imageUrl: "../assets/avatar2.png" },
-    { id: "3", name: "Bob", imageUrl: "../assets/avatar3.png" },
-    { id: "4", name: "Alice", imageUrl: "../assets/avatar4.png" },
-    { id: "5", name: "Mike", imageUrl: "../assets/avatar5.png" },
-    { id: "6", name: "Emily", imageUrl: "../assets/avatar6.png" },
-    { id: "7", name: "David", imageUrl: "../assets/avatar7.png" },
-    { id: "8", name: "Sarah", imageUrl: "../assets/avatar8.png" },
-    { id: "9", name: "Michael", imageUrl: "../assets/avatar9.png" },
-    { id: "10", name: "Olivia", imageUrl: "../assets/avatar10.png" },
-    { id: "11", name: "Daniel", imageUrl: "../assets/avatar11.png" },
-    { id: "12", name: "Emma", imageUrl: "../assets/avatar12.png" },
+    { id: "1", name: "John", imageUrl: require("../assets/avatar1.png") },
+    { id: "2", name: "Jane", imageUrl: require("../assets/avatar2.png") },
+    { id: "3", name: "Bob", imageUrl: require("../assets/avatar3.png") },
+    { id: "4", name: "Alice", imageUrl: require("../assets/avatar4.png") },
+    { id: "5", name: "Bob", imageUrl: require("../assets/avatar5.png") },
+    { id: "6", name: "Bob", imageUrl: require("../assets/avatar6.png") },
+    { id: "7", name: "Bob", imageUrl: require("../assets/avatar7.png") },
+    { id: "8", name: "Bob", imageUrl: require("../assets/avatar8.png") },
+    { id: "9", name: "Bob", imageUrl: require("../assets/avatar9.png") },
+    { id: "10", name: "Bob", imageUrl: require("../assets/avatar10.png") },
+    { id: "11", name: "Bob", imageUrl: require("../assets/avatar11.png") },
+    { id: "12", name: "Bob", imageUrl: require("../assets/avatar12.png") },
   ]
+  const [userInput, setUserInput] = useState("")
+  const [selectedAvatar, setSelectedAvatar] = useState<DataAvatar | null>(null)
+  // const renderAvatar = ({ item }: { item: DataAvatar }) => (
+  //   <Avatar imageUrl={item.imageUrl} name={item.name} />
+  // )
 
-  const renderAvatar = ({ item }: { item: DataAvatar }) => (
-    <Avatar imageUrl={item.imageUrl} name={item.name} />
-  )
+  const handleUpdate = async () => {
+    try {
+      if (selectedAvatar) {
+        const { id, imageUrl } = selectedAvatar
+        const response = await axios.patch(
+          "http://localhost:8000/api/v1/updateUser",
+          {
+            name: userInput,
+            avatar: { id, imageUrl },
+          }
+        )
+        console.log("Update successful", response.data)
+      } else {
+        console.error("Please select an avatar")
+      }
+    } catch (error) {
+      console.error("Update failed", error)
+    }
+  }
+
+  const handleTextChange = (text: string) => {
+    setUserInput(text)
+    console.log("Input Text:", text)
+  }
   return (
     <View style={styles.container}>
       <Image style={styles.background} source={require("../assets/bg2.png")} />
@@ -48,78 +73,14 @@ const Profile = () => {
       {/* <View style={styles.avatar}></View> */}
       <Text style={styles.textup}>Select Your Avatar</Text>
       <View style={styles.grid}>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar1.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar2.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar3.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar4.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar5.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar6.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar7.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar8.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar9.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar10.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar11.png")}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={styles.avatar}
-            source={require("../assets/avatar12.png")}
-          />
-        </TouchableOpacity>
+        {data.map((item) => (
+          <TouchableOpacity
+            key={item.id}
+            onPress={() => setSelectedAvatar(item)}
+          >
+            <Image style={styles.avatar} source={{ uri: item.imageUrl }} />
+          </TouchableOpacity>
+        ))}
       </View>
       <View style={{ flex: 1, marginTop: -200 }}>
         <View style={styles.inputContainer}>
@@ -127,15 +88,10 @@ const Profile = () => {
             style={styles.input}
             placeholder="Your Name"
             placeholderTextColor="gray"
+            onChangeText={handleTextChange}
           />
-          {/* <MaterialIcons
-            name="drive-file-rename-outline"
-            size={30}
-            color="black"
-            style={styles.icon}
-          /> */}
         </View>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleUpdate}>
           <Text style={styles.text}>Continue</Text>
         </TouchableOpacity>
       </View>
