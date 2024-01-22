db825bdca3586c2072112a497e5a28bf77a9ae1a
wira97-tech <leonkmoena@gmail.com>
2024-01-22 08:59:52 +0700

    update

--- Frontend/pages/Podium.tsx
+++ Frontend/pages/Podium.tsx
@@ -1,85 +1,94 @@
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Image, Text } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { initializeSocket } from "../utils/socket";
+import React, { useState, useEffect } from "react"
+import { View, StyleSheet, Image, Text } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { initializeSocket } from "../utils/socket"
 
 const Podium = () => {
-  const socket = initializeSocket();
-  const navigate = useNavigation();
-  const [data, setData] = useState([]);
+  const socket = initializeSocket()
+  const navigate = useNavigation()
+  const [data, setData] = useState([])
 
   const StartNavigate = () => {
-    navigate.navigate("StartGame" as never);
-  };
+    navigate.navigate("StartGame" as never)
+  }
 
   useEffect(() => {
-    socket.emit("user", {score: 0});
+    socket.emit("user", { score: 0 })
     socket.on("user", (user) => {
-      setData(user.sort((a:any, b:any) => b.score - a.score));
+      setData(user.sort((a: any, b: any) => b.score - a.score))
     })
-  }, []);
+  }, [StartNavigate])
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Image style={styles.background} source={require("../assets/bg2.png")} />
 
       <View style={{ alignItems: "center" }}>
-          <Text style={{ color: "white", fontSize: 35, fontWeight: "bold", marginTop: 30 }}>Congratulations!</Text>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 35,
+            fontWeight: "bold",
+            marginTop: 30,
+          }}
+        >
+          Congratulations!
+        </Text>
 
-          {data.length !== 0 &&
-            data.map((user:any, index) => (
-              <>
-                {index == 0 && (
-                  <View
+        {data.length !== 0 &&
+          data.map((user: any, index) => (
+            <>
+              {index == 0 && (
+                <View
+                  style={{
+                    marginTop: 20,
+                    backgroundColor: "#F4CE14",
+                    paddingHorizontal: 40,
+                    paddingVertical: 20,
+                    borderRadius: 10,
+                    marginBottom: 13,
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 20,
+                  }}
+                >
+                  <Image
+                    source={user.avatar}
                     style={{
-                      marginTop: 20,
-                      backgroundColor: "#F4CE14",
-                      paddingHorizontal: 40,
-                      paddingVertical: 20,
-                      borderRadius: 10,
-                      marginBottom: 13,
-                      display: "flex",
-                      flexDirection: "row",
-                      alignItems: "center",
-                      gap: 20,
+                      width: 130,
+                      height: 130,
+                      borderRadius: 65,
+                      borderColor: "white",
+                      borderWidth: 2,
                     }}
-                  >
-                    <Image
-                      source={user.avatar}
-                      style={{
-                        width: 130,
-                        height: 130,
-                        borderRadius: 65,
-                        borderColor: "white",
-                        borderWidth: 2,
-                      }}
-                    />
-                    <Text style={styles.nameText}>{user.name}</Text>
-                  </View>
-                )}
+                  />
+                  <Text style={styles.nameText}>{user.name}</Text>
+                </View>
+              )}
 
-                {index !== 0 && (
-                  <View style={[styles.optionButton]}>
-                    <Image
-                      source={user.avatar}
-                      style={{
-                        width: 50,
-                        height: 50,
-                        borderRadius: 100,
-                        marginRight: 20,
-                        borderColor: "white",
-                        borderWidth: 2,
-                      }}
-                    />
-                    <View style={{ marginLeft: 5 }}>
-                      <Text style={styles.optionText}>{user.name}</Text>
-                      <Text style={styles.scoreText}>Score: {user.score}</Text>
-                    </View>
+              {index !== 0 && (
+                <View style={[styles.optionButton]}>
+                  <Image
+                    source={user.avatar}
+                    style={{
+                      width: 50,
+                      height: 50,
+                      borderRadius: 100,
+                      marginRight: 20,
+                      borderColor: "white",
+                      borderWidth: 2,
+                    }}
+                  />
+                  <View style={{ marginLeft: 5 }}>
+                    <Text style={styles.optionText}>{user.name}</Text>
+                    <Text style={styles.scoreText}>Score: {user.score}</Text>
                   </View>
-                )}
-              </>
-            ))}
-        </View>
+                </View>
+              )}
+            </>
+          ))}
+      </View>
 
       {/* <View style={{ alignItems: "center" }}>
         <View
@@ -240,10 +249,10 @@ const Podium = () => {
           />
         </View> */}
     </View>
-  );
-};
+  )
+}
 
-export default Podium;
+export default Podium
 const styles = StyleSheet.create({
   background: {
     position: "absolute",
@@ -310,4 +319,4 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
   },
-});
+})
